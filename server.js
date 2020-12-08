fbb2c2534dbde648caa9d9e2f8a07be9e74ed25c
Mesut Beysülen <mesut.beysulen@hepsiburada.com>
2020-12-08 21:41:47 +0300

    ios için iosreviewlist ve iosappinfo servisleri eklendi. androidratings model oluşturuldu.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var gplay = require('google-play-scraper');
+var appStore = require('app-store-scraper');
+let page;
 let limit;
 let sorting;
 const hostname = 'localhost';
@@ -9,7 +11,7 @@ let nextPaginationToken;
 
 
 //Get all review by limit default = 150
-app.get('/reviewList', function (req, res, next) {
+app.get('/androidreviewlist', function (req, res, next) {
 
     limit = req.query.limit;
     sorting = req.query.sorting;
@@ -17,9 +19,9 @@ app.get('/reviewList', function (req, res, next) {
     packageName = req.query.packageName;
 
 
-    if (nextPaginationToken == "default") {
+    if (nextPaginationToken === "default") {
         console.log(nextPaginationToken + "null olana girdi!")
-        if (sorting == 'false' || sorting == "newest") {
+        if (sorting === 'false' || sorting === "newest") {
             gplay.reviews({
                 appId: packageName,
                 sort: gplay.sort.NEWEST,
@@ -30,18 +32,10 @@ app.get('/reviewList', function (req, res, next) {
                 let data = result.data;
                 nextPaginationToken = result.nextPaginationToken;
                 try {
-                    if (result != null) {
-                        res.setHeader('Content-Type', 'application/json');
-                        res.send(JSON.stringify({ data: data, nextPaginationToken: nextPaginationToken }));
-                        console.log("Got a GET request for list");
-                        next();
-                    }
-                    else {
-                        res.setHeader('Content-Type', 'application/json');
-                        res.send(JSON.stringify({ success: false, response: "Empty" }));
-                        console.log("Got a GET request for list but list empty");
-                        next();
-                    }
+                    res.setHeader('Content-Type', 'application/json');
+                    res.send(JSON.stringify({data: data, nextPaginationToken: nextPaginationToken}));
+                    console.log("Got a GET request for list");
+                    next();
                 }
                 catch (err) {
                     res.status(500) //Internal Server Error
@@ -50,7 +44,7 @@ app.get('/reviewList', function (req, res, next) {
                 }
             });
         }
-        else if (sorting == "rating") {
+        else if (sorting === "rating") {
 
             gplay.reviews({
                 appId: packageName,
@@ -62,18 +56,10 @@ app.get('/reviewList', function (req, res, next) {
                 let data = result.data;
                 let nextPaginationToken = result.nextPaginationToken;
                 try {
-                    if (result != null) {
-                        res.setHeader('Content-Type', 'application/json');
-                        res.send(JSON.stringify({ data: data, nextPaginationToken: nextPaginationToken }));
-                        console.log("Got a GET request for list");
-                        next();
-                    }
-                    else {
-                        res.setHeader('Content-Type', 'application/json');
-                        res.send(JSON.stringify({ success: false, response: "Empty" }));
-                        console.log("Got a GET request for list but list empty");
-                        next();
-                    }
+                    res.setHeader('Content-Type', 'application/json');
+                    res.send(JSON.stringify({data: data, nextPaginationToken: nextPaginationToken}));
+                    console.log("Got a GET request for list");
+                    next();
                 }
                 catch (err) {
                     res.status(500) //Internal Server Error
@@ -82,7 +68,7 @@ app.get('/reviewList', function (req, res, next) {
                 }
             });
         }
-        else if (sorting == "helpfulness") {
+        else if (sorting === "helpfulness") {
             gplay.reviews({
                 appId: packageName,
                 sort: gplay.sort.HELPFULNESS,
@@ -93,18 +79,10 @@ app.get('/reviewList', function (req, res, next) {
                 let data = result.data;
                 let nextPaginationToken = result.nextPaginationToken;
                 try {
-                    if (result != null) {
-                        res.setHeader('Content-Type', 'application/json');
-                        res.send(JSON.stringify({ data: data, nextPaginationToken: nextPaginationToken }));
-                        console.log("Got a GET request for list");
-                        next();
-                    }
-                    else {
-                        res.setHeader('Content-Type', 'application/json');
-                        res.send(JSON.stringify({ success: false, response: "Empty" }));
-                        console.log("Got a GET request for list but list empty");
-                        next();
-                    }
+                    res.setHeader('Content-Type', 'application/json');
+                    res.send(JSON.stringify({data: data, nextPaginationToken: nextPaginationToken}));
+                    console.log("Got a GET request for list");
+                    next();
                 }
                 catch (err) {
                     res.status(500) //Internal Server Error
@@ -118,7 +96,7 @@ app.get('/reviewList', function (req, res, next) {
 
     else {
         console.log("null olmayana girdi!")
-        if (sorting == 'false' || sorting == "newest") {
+        if (sorting === 'false' || sorting === "newest") {
             gplay.reviews({
                 appId: packageName,
                 sort: gplay.sort.NEWEST,
@@ -131,18 +109,10 @@ app.get('/reviewList', function (req, res, next) {
                 let data = result.data;
                 nextPaginationToken = result.nextPaginationToken;
                 try {
-                    if (result != null) {
-                        res.setHeader('Content-Type', 'application/json');
-                        res.send(JSON.stringify({ data: data, nextPaginationToken: nextPaginationToken }));
-                        console.log("Got a GET request for list");
-                        next();
-                    }
-                    else {
-                        res.setHeader('Content-Type', 'application/json');
-                        res.send(JSON.stringify({ success: false, response: "Empty" }));
-                        console.log("Got a GET request for list but list empty");
-                        next();
-                    }
+                    res.setHeader('Content-Type', 'application/json');
+                    res.send(JSON.stringify({data: data, nextPaginationToken: nextPaginationToken}));
+                    console.log("Got a GET request for list");
+                    next();
                 }
                 catch (err) {
                     res.status(500) //Internal Server Error
@@ -151,7 +121,7 @@ app.get('/reviewList', function (req, res, next) {
                 }
             });
         }
-        else if (sorting == "rating") {
+        else if (sorting === "rating") {
 
             gplay.reviews({
                 appId: packageName,
@@ -165,18 +135,10 @@ app.get('/reviewList', function (req, res, next) {
                 let data = result.data;
                 let nextPaginationToken = result.nextPaginationToken;
                 try {
-                    if (result != null) {
-                        res.setHeader('Content-Type', 'application/json');
-                        res.send(JSON.stringify({ data: data, nextPaginationToken: nextPaginationToken }));
-                        console.log("Got a GET request for list");
-                        next();
-                    }
-                    else {
-                        res.setHeader('Content-Type', 'application/json');
-                        res.send(JSON.stringify({ success: false, response: "Empty" }));
-                        console.log("Got a GET request for list but list empty");
-                        next();
-                    }
+                    res.setHeader('Content-Type', 'application/json');
+                    res.send(JSON.stringify({data: data, nextPaginationToken: nextPaginationToken}));
+                    console.log("Got a GET request for list");
+                    next();
                 }
                 catch (err) {
                     res.status(500) //Internal Server Error
@@ -185,7 +147,7 @@ app.get('/reviewList', function (req, res, next) {
                 }
             });
         }
-        else if (sorting == "helpfulness") {
+        else if (sorting === "helpfulness") {
             gplay.reviews({
                 appId: packageName,
                 sort: gplay.sort.HELPFULNESS,
@@ -198,18 +160,10 @@ app.get('/reviewList', function (req, res, next) {
                 let data = result.data;
                 let nextPaginationToken = result.nextPaginationToken;
                 try {
-                    if (result != null) {
-                        res.setHeader('Content-Type', 'application/json');
-                        res.send(JSON.stringify({ data: data, nextPaginationToken: nextPaginationToken }));
-                        console.log("Got a GET request for list");
-                        next();
-                    }
-                    else {
-                        res.setHeader('Content-Type', 'application/json');
-                        res.send(JSON.stringify({ success: false, response: "Empty" }));
-                        console.log("Got a GET request for list but list empty");
-                        next();
-                    }
+                    res.setHeader('Content-Type', 'application/json');
+                    res.send(JSON.stringify({data: data, nextPaginationToken: nextPaginationToken}));
+                    console.log("Got a GET request for list");
+                    next();
                 }
                 catch (err) {
                     res.status(500) //Internal Server Error
@@ -222,6 +176,118 @@ app.get('/reviewList', function (req, res, next) {
     }
 
 })
+
+//ANDROid rating bilgilewrini döndürür.
+app.get('/androidratings',function (req,res, next) {
+
+})
+
+//iOS yorum listesi döndürülür
+app.get('/iosreviewlist', function (req, res, next) {
+
+    sorting = req.query.sorting;
+    appId = req.query.packageId;
+    pageNo = req.query.page;
+    if (pageNo<=10){
+        if (sorting === "recent") {
+            appStore.reviews({
+                appId: 'com.pozitron.hepsiburada',
+                sort: appStore.sort.HELPFUL,
+                page: pageNo
+            }).then(function (result) {
+                let data = result;
+                try {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.send(JSON.stringify({data: data}));
+                    console.log("Got a GET request for list");
+                    next();
+                } catch (err) {
+                    res.status(500) //Internal Server Error
+                    res.send(JSON.stringify({success: false, message: err.message}));
+                    console.log("Not got a GET request for list");
+                }
+            });
+
+        }
+        else {
+            appStore.reviews({
+                appId: 'com.pozitron.hepsiburada',
+                sort: appStore.sort.HELPFUL,
+                page: pageNo
+            }).then(function (result) {
+                let data = result;
+                try {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.send(JSON.stringify({data: data}));
+                    console.log("Got a GET request for list");
+                    next();
+                } catch (err) {
+                    res.status(500) //Internal Server Error
+                    res.send(JSON.stringify({success: false, message: err.message}));
+                    console.log("Not got a GET request for list");
+                }
+            });
+
+        }
+    }
+    else {
+        res.status(400)
+        res.send(JSON.stringify({success: false, message: "iosreviewlist is limited to 10 page"}));
+    }
+
+
+})
+
+//ios app bilgisi döndürülür - ratings=true ise rating ve histogram bilgisi döndürülür.
+app.get('/iosappinfo',function (req,res, next) {
+    appId = req.query.packageId;
+    scoreRate = req.query.rating;
+    if(scoreRate === 'true'){
+        appStore.app({
+            id: appId,
+            ratings: true
+        }).then(function (result) {
+            let data = result;
+            try {
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify({data: data}));
+                console.log("Got a GET request for list");
+                next();
+            } catch (err) {
+                res.status(500) //Internal Server Error
+                res.send(JSON.stringify({success: false, message: err.message}));
+                console.log("Not got a GET request for list");
+            }
+        }).catch(function (err) {
+            res.status(500) //Internal Server Error
+            res.send(JSON.stringify({success: false, message: err.message}));
+            console.log("Not got a GET request for list");
+        });
+    }
+    else {
+        appStore.app({
+            id: appId
+        }).then(function (result) {
+            let data = result;
+            try {
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify({data: data}));
+                console.log("Got a GET request for list");
+                next();
+            } catch (err) {
+                res.status(500) //Internal Server Error
+                res.send(JSON.stringify({success: false, message: err.message}));
+                console.log("Not got a GET request for list");
+            }
+        }).catch(function (err) {
+            res.status(500) //Internal Server Error
+            res.send(JSON.stringify({success: false, message: err.message}));
+            console.log("Not got a GET request for list");
+        });
+    }
+
+})
+
 
 
 app.listen(port, function () {
